af272a30c01a2ffa178b117204bfe66eedc8489d
marekboro <marek.borowicz83@gmail.com>
2020-08-19 21:05:46 +0100

    Frist test passing

--- hw_advanced_enumeration_start/isogram_finder/isogram_finder.js
+++ hw_advanced_enumeration_start/isogram_finder/isogram_finder.js
@@ -1,8 +1,32 @@
 const IsogramFinder = function (word) {
+    this.word = word;
 
 }
-//! ISOGRAM is a string where each character can occur only once.
+//! An ISOGRAM is a string where each character can occur only once.
 IsogramFinder.prototype.isIsogram = function () {
+// one approach will be to split a string into individual characters and based on these create keys that increment, much like one of the hw extension demonstrated today. 
+// once this is acomplised, loop over the values for the keys and if anything is over 1, false, otherwise true. 
+
+wordLowerSplit = this.word.toLowerCase().split('');
+
+const letterCount = {};
+for (const character of wordLowerSplit) {
+    if(letterCount[character]){
+        letterCount[character] +=1;
+    }
+    else {
+        letterCount[character]=1;
+    }
+}
+
+for (var key in letterCount){
+    if(letterCount[key] > 1){
+        return false;
+    }
+    else {
+        return true;
+    }
+}
 
 }
 
