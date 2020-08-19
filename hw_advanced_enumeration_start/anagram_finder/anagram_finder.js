ca3880dbd68390daea336b0effc374f0ca12ab83
marekboro <marek.borowicz83@gmail.com>
2020-08-19 21:25:08 +0100

    could it be this simple?

--- hw_advanced_enumeration_start/anagram_finder/anagram_finder.js
+++ hw_advanced_enumeration_start/anagram_finder/anagram_finder.js
@@ -1,9 +1,24 @@
 const AnagramFinder = function (word) {
-
+    this.word = word;
 }
 
 AnagramFinder.prototype.findAnagrams = function (otherWords) {
 
+//! an ANAGRAMS are words who have the same characters and same number of characters
+// will borrow code from isogram to create object from word and create objects of each word in otherWords,
+// will have an emty array that will get populated when there is a match.
+//! This is a mess...maybe there is a simpler way.
+
+//! sort a string, then compare it! - much simpler.
+const anagrams = [];
+sortedWord = this.word.sort().toLowerCase();
+
+for (checkedWord of otherWords){
+    if(checkedWord.sort().toLowerCase() === sortedWord) {
+        anagrams.push(checkedWord);
+    }
+}
+return anagrams;
 }
 
 module.exports = AnagramFinder;
